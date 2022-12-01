5837335e9e5031ddfd718c0851e2a10d8f0d121c
David Humphrey <david.andrew.humphrey@gmail.com>
2022-12-01 13:11:08 -0500

    Add support for creating subdomains from web UI

--- starchart/src/acme.js
+++ starchart/src/acme.js
@@ -4,14 +4,13 @@
  */
 
 // const fs = require('fs').promises;
-const acme = require('acme-client');
+const acme = require("acme-client");
 
-const dns = require('./dns');
+const dns = require("./dns");
 
 function log(m) {
-    process.stdout.write(`${m}\n`);
+  process.stdout.write(`${m}\n`);
 }
-
 
 /**
  * Function used to satisfy an ACME challenge
@@ -23,15 +22,14 @@ function log(m) {
  */
 
 function challengeCreateFn(authz, challenge, keyAuthorization) {
-    log('Triggered challengeCreateFn()');
+  log("Triggered challengeCreateFn()");
 
-    const dnsRecord = `_acme-challenge.${authz.identifier.value}`;
-    const recordValue = keyAuthorization;
+  const dnsRecord = `_acme-challenge.${authz.identifier.value}`;
+  const recordValue = keyAuthorization;
 
-    log(`Creating TXT record for ${authz.identifier.value}: ${dnsRecord}`);
-    return dns.createRecord(dnsRecord, 'TXT', recordValue);
+  log(`Creating TXT record for ${authz.identifier.value}: ${dnsRecord}`);
+  return dns.createTxtRecord(dnsRecord, recordValue);
 }
-
 
 /**
  * Function used to remove an ACME challenge response
@@ -43,65 +41,64 @@ function challengeCreateFn(authz, challenge, keyAuthorization) {
  */
 
 function challengeRemoveFn(authz, challenge, keyAuthorization) {
-    log('Triggered challengeRemoveFn()');
+  log("Triggered challengeRemoveFn()");
 
-    const dnsRecord = `_acme-challenge.${authz.identifier.value}`;
-    const recordValue = keyAuthorization;
+  const dnsRecord = `_acme-challenge.${authz.identifier.value}`;
+  const recordValue = keyAuthorization;
 
-    log(`Removing TXT record for ${authz.identifier.value}: ${dnsRecord}`);
-    return dns.deleteRecord(dnsRecord, 'TXT', recordValue);
+  log(`Removing TXT record for ${authz.identifier.value}: ${dnsRecord}`);
+  return dns.deleteTxtRecord(dnsRecord, recordValue);
 }
-
 
 /**
  * Main
  */
 
 module.exports = async () => {
-    // Domain
-    const domain = '*.starchart.invalid';
+  // Domain
+  const domain = "*.username.starchart.invalid";
 
-    // Assumes pebble is running in docker already
-    const pebbleDirUrl = 'https://127.0.0.1:14000/dir';
+  // Assumes pebble is running in docker already
+  const pebbleDirUrl = "https://127.0.0.1:14000/dir";
 
-    let directoryUrl = process.env.ACME_DIRECTORY_URL;
-    if(!directoryUrl) {
-        console.warn(`Missing ACME_DIRECTORY_URL, using ${pebbleDirUrl}`);
-        directoryUrl = pebbleDirUrl;
-    }
+  let directoryUrl = process.env.ACME_DIRECTORY_URL;
+  if (!directoryUrl) {
+    console.warn(`Missing ACME_DIRECTORY_URL, using ${pebbleDirUrl}`);
+    directoryUrl = pebbleDirUrl;
+  }
 
-    /* Init client */
-    const client = new acme.Client({
-        directoryUrl,
-        accountKey: await acme.crypto.createPrivateKey()
-    });
+  /* Init client */
+  const client = new acme.Client({
+    directoryUrl,
+    accountKey: await acme.crypto.createPrivateKey(),
+  });
 
-    /* Create Certificate Signing Request */
-    const [key, csr] = await acme.crypto.createCsr({
-        commonName: domain,
-    });
+  /* Create Certificate Signing Request */
+  const [key, csr] = await acme.crypto.createCsr({
+    commonName: domain,
+  });
 
-    /* Certificate */
-    const cert = await client.auto({
-        csr,
-        email: 'mail@starchart.invalid',
-        termsOfServiceAgreed: true,
-        challengePriority: ['dns-01'],
-        // Just for testing locally...
-        skipChallengeVerification: true,
-        challengeCreateFn,
-        challengeRemoveFn
-    });
+  /* Certificate */
+  const cert = await client.auto({
+    csr,
+    email: "mail@starchart.invalid",
+    termsOfServiceAgreed: true,
+    challengePriority: ["dns-01"],
+    // Just for testing locally...
+    skipChallengeVerification: true,
+    challengeCreateFn,
+    challengeRemoveFn,
+  });
 
-    /* Done */
-    log(`CSR:\n${csr.toString()}\n`);
-    log(`Private key:\n${key.toString()}\n`);
-    log(`Certificate:\n${cert.toString()}\n`);
+  /* Done */
+  log(`CSR:\n${csr.toString()}\n`);
+  log(`Private key:\n${key.toString()}\n`);
+  log(`Certificate:\n${cert.toString()}\n`);
 
-    return {
-        domain,
-        csr: csr.toString(),
-        privateKey: key.toString(),
-        cert: cert.toString(),
-    };
+  return {
+    domain,
+    csr: csr.toString(),
+    privateKey: key.toString(),
+    cert: cert.toString(),
+  };
 };
--- starchart/src/dns.js
+++ starchart/src/dns.js
@@ -8,94 +8,129 @@
  */
 
 // TODO: hard-coding for now, see config/primary/pdns.conf
-const POWERDNS_API_KEY = process.env.POWERDNS_API_KEY || 'secret-api-key';
-const POWERDNS_API_URL = process.env.POWERDNS_API_URL || 'http://localhost:8081/api/v1';
+const POWERDNS_API_KEY = process.env.POWERDNS_API_KEY || "secret-api-key";
+const POWERDNS_API_URL =
+  process.env.POWERDNS_API_URL || "http://localhost:8081/api/v1";
 
 // The Pebble Challenge Server mocking DNS records for the fake Let's Encrypt
-const PEBBLE_MOCK_DNS_URL = process.env.PEBBLE_MOCK_DNS_URL || 'http://localhost:8055';
+const PEBBLE_MOCK_DNS_URL =
+  process.env.PEBBLE_MOCK_DNS_URL || "http://localhost:8055";
 
-// Our domain (we don't own this, just for testing) 
-const STARCHART_ZONE = 'starchart.invalid';
+// Our domain (we don't own this, just for testing)
+const STARCHART_ZONE = "starchart.invalid";
 
 // Make sure a string ends with a `.`, adding if missing
-const endsWithPeriod = s => s.replace(/\.?$/, '.');
+const endsWithPeriod = (s) => s.replace(/\.?$/, ".");
+
+// Wrap a string in double-quotes
+const dbquote = (s) => `"${s}"`;
 
 // Resource Record Set
 // https://doc.powerdns.com/authoritative/http-api/zone.html#rrset
 class RRSet {
-    constructor({ type = 'TXT', changeType, dnsRecord, recordValue }) {
-        // Canonical names must end in `.`
-        this.name = endsWithPeriod(dnsRecord);
-        this.type = type;
-        this.changetype = changeType;
-        if(changeType !== 'delete') {
-            this.ttl = 3600;
-        }
-        this.records = [{
-            // TXT record values must be wrapped in "..."
-            content: `"${recordValue}"`,
-            disabled: false,
-        }];
+  constructor({ changeType, dnsRecord, recordValue, dnsRecordType }) {
+    // Canonical names must end in `.`
+    this.name = dnsRecord;
+    this.type = dnsRecordType;
+    this.changetype = changeType;
+    if (changeType !== "delete") {
+      this.ttl = 3600;
     }
+    this.records = [
+      {
+        // TXT record values must be wrapped in "..."
+        content: dnsRecordType === "TXT" ? dbquote(recordValue) : recordValue,
+        disabled: false,
+      },
+    ];
+  }
 }
 
-async function patchRecord(changeType, dnsRecord, recordValue) {
-    const rrset = new RRSet({ changeType, dnsRecord, recordValue });
+async function patchRecord(changeType, dnsRecord, recordValue, dnsRecordType) {
+  const rrset = new RRSet({
+    changeType,
+    dnsRecord,
+    recordValue,
+    dnsRecordType,
+  });
 
-    // https://doc.powerdns.com/authoritative/http-api/zone.html#patch--servers-server_id-zones-zone_id
-    const res = await fetch(`${POWERDNS_API_URL}/servers/localhost/zones/${STARCHART_ZONE}.`, {
-        method: 'PATCH',
-        headers: {
-            'X-API-Key': POWERDNS_API_KEY,
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify({ rrsets: [ rrset ] }),
-    });
+  // https://doc.powerdns.com/authoritative/http-api/zone.html#patch--servers-server_id-zones-zone_id
+  const res = await fetch(
+    `${POWERDNS_API_URL}/servers/localhost/zones/${STARCHART_ZONE}.`,
+    {
+      method: "PATCH",
+      headers: {
+        "X-API-Key": POWERDNS_API_KEY,
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({ rrsets: [rrset] }),
+    }
+  );
 
-    // Success will be a 204. Log an error otherwise
-    if(res.status !== 204) {
-        try {
-            console.warn('Error from PowerDNS API', await res.json());
-        } catch(_) {
-            // 404s don't seem to come back as JSON :(
-            console.warn(await res.text());
-        } finally {
-            throw new Error('Unable to create DNS record');
-        }
-    }    
+  // Success will be a 204. Log an error otherwise
+  if (res.status !== 204) {
+    try {
+      const { error } = await res.json();
+      console.warn("Error from PowerDNS API", error);
+      throw new Error(`Unable to create DNS record: ${error}`);
+    } catch (_) {
+      // 404s don't seem to come back as JSON :(
+        throw new Error("Unable to create DNS record");
+    }
+  }
 }
 
 // Create a TXT record "_acme-challenge.starchart.invalid" with value "_v3lO9-X8YIz66WVylZaxWYZI8bYetULl0vz8GeTZeY"
-module.exports.createRecord = (dnsRecord, type = 'TXT', recordValue) => {
-    return Promise.all([
-        // Update PowerDNS
-        patchRecord('replace', dnsRecord, recordValue),
-        // Also add a mock DNS entry to our Pebble Challenge Server
-        // https://github.com/letsencrypt/pebble/tree/main/cmd/pebble-challtestsrv#dns-01
-        fetch(`${PEBBLE_MOCK_DNS_URL}/set-txt`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-            },
-            body: JSON.stringify({ host: endsWithPeriod(dnsRecord), value: recordValue }),
-        })
-    ]);
+module.exports.createTxtRecord = (dnsRecord, recordValue) => {
+  return Promise.all([
+    // Update PowerDNS
+    patchRecord("replace", endsWithPeriod(dnsRecord), recordValue, "TXT"),
+    // Also add a mock DNS entry to our Pebble Challenge Server
+    // https://github.com/letsencrypt/pebble/tree/main/cmd/pebble-challtestsrv#dns-01
+    fetch(`${PEBBLE_MOCK_DNS_URL}/set-txt`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({
+        host: endsWithPeriod(dnsRecord),
+        value: recordValue,
+      }),
+    }),
+  ]);
 };
 
-module.exports.deleteRecord = (dnsRecord, type = 'TXT', recordValue) => {
-    return Promise.all([
-        // Update PowerDNS
-        // NOTE: ignoring this for now so record stays in pdns
-        // patchRecord('delete', dnsRecord, recordValue),
-  
-        // Also add a mock DNS entry to our Pebble Challenge Server
-        // https://github.com/letsencrypt/pebble/tree/main/cmd/pebble-challtestsrv#dns-01
-        fetch(`${PEBBLE_MOCK_DNS_URL}/clear-txt`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-            },
-            body: JSON.stringify({ host: endsWithPeriod(dnsRecord) }),
-        })
-    ]);
+module.exports.deleteTxtRecord = (dnsRecord, recordValue) => {
+  return Promise.all([
+    // Update PowerDNS
+    // NOTE: ignoring this for now so record stays in pdns
+    // patchRecord('delete', endsWithPeriod(dnsRecord), recordValue, 'TXT'),
+
+    // Also add a mock DNS entry to our Pebble Challenge Server
+    // https://github.com/letsencrypt/pebble/tree/main/cmd/pebble-challtestsrv#dns-01
+    fetch(`${PEBBLE_MOCK_DNS_URL}/clear-txt`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({ host: endsWithPeriod(dnsRecord) }),
+    }),
+  ]);
+};
+
+module.exports.createSubdomain = async function (subdomain, type, value) {
+  if (!(type === "A" || type === "CNAME")) {
+    throw new Error("Expected A or CNAME for `type`");
+  }
+  if (typeof subdomain !== "string" && subdomain.length) {
+    throw new Error("Expected string for `name`");
+  }
+
+  const domain = `${subdomain}.username.starchart.invalid`;
+  if(type === 'CNAME') {
+    // Make canonical by adding a trailing period
+    value = endsWithPeriod(value);
+  }
+  await patchRecord("replace", endsWithPeriod(domain), value, type);
+  return domain;
 };
